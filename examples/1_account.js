c17f6c40733ca4de752d81cb49479e74b8678cdf
Pelz <alongepelumi15@gmail.com>
2023-08-12 05:01:27 +0100

    modified: 1_account.js

--- examples/1_account.js
+++ examples/1_account.js
@@ -1,8 +1,8 @@
 const { ethers } = require("ethers");
 
-const ALCHEMY_ID = "";
+const ALCHEMY_ID = "RaPCx4JgXvplxH-bHQaNkh42750329Ze";
 const provider = new ethers.JsonRpcProvider(
-  `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_ID}`
+  `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ID}`
 );
 
 const address = "0x915bFb22C3fe6E3Dd51c048bF54deDb085B8f1e6";
@@ -10,7 +10,7 @@ const main = async () => {
   console.log("================Running================");
   const balance = await provider.getBalance(address);
   console.log(
-    `Eth balance of ${address} is: ${ethers.utils.formatEther(balance)} ETH`
+    `Eth balance of ${address} is: ${ethers.formatEther(balance)} ETH`
   );
 };
 
