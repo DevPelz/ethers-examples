dfa513d2cd26ad17a7de02f2754ad1cf6372fdd1
Pelz <alongepelumi15@gmail.com>
2023-08-12 04:03:05 +0100

    initial commit

---
+++ examples/1_account.js
@@ -0,0 +1,17 @@
+const { ethers } = require("ethers");
+
+const ALCHEMY_ID = "";
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_ID}`
+);
+
+const address = "0x915bFb22C3fe6E3Dd51c048bF54deDb085B8f1e6";
+const main = async () => {
+  console.log("================Running================");
+  const balance = await provider.getBalance(address);
+  console.log(
+    `Eth balance of ${address} is: ${ethers.utils.formatEther(balance)} ETH`
+  );
+};
+
+main();
